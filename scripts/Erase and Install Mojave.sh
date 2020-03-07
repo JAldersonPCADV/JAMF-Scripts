0917e64f044ceed96c4b97b36bd7c5b8846834b8
Jarad Alderson <jalderson@pcadv.org>
2020-03-07 13:52:19 -0800

    Updated Application Path

--- scripts/Erase and Install Mojave.sh
+++ scripts/Erase and Install Mojave.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #[ -f /Applications/Install\ macOS\ Mojave.app ] && echo "Mojave Installer is present" || echo "Mojave Installer Needed"
 
-echo "<result>$(file="/Library/LaunchDaemons/com.kolide-k2.launcher.plist"
+echo "<result>$(file="/Applications/Contacts.app"
 if  test -s "$file" 
 then 
     echo "Mojave Installer is present"
 else 
     echo "Mojave Installer Needed"
 fi)</result>"
+exit
